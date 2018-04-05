6512b8bf4b7f74b1557f83e771ccd2d1adba478a
Usuario <usuario@MacBook-Pro-de-Eduardo.local>
2018-04-05 14:11:01 -0300

    se corrige JWT auth

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const app = express();
 //usamos middelware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();)
 app.use('/api', api)
 app.use(cors())
 
